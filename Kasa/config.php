c6b3a62c0dd094d10d9349864029a51806c3260b
VerSus146 <versus146pl@gmail.com>
2018-01-23 19:14:33 +0100

    Działa

--- Kasa/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-mysql_connect('sql.versus146.nazwa.pl', 'versus146_DomowaKasa', 'Andertejl146');
-
-mysql_select_db('versus146_DomowaKasa') 
-or die('Nie mogę połączyć się z bazą danych<br />Błąd: '.mysql_error());
-?>
